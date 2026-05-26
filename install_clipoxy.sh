6c61ce9952e08a2a3ba0e5c4fcea716fdd0b9e86
小宝软件 <305230354@qq.com>
2026-05-26 15:11:23 +0800

    fix

--- install_clipoxy.sh
+++ install_clipoxy.sh
@@ -75,7 +75,7 @@ if [ "$CHOICE" = "1" ]; then
         cd CLIProxyAPIPlus && git pull
     else
         rm -rf CLIProxyAPIPlus
-        git clone https://github.com/zczy-k/CLIProxyAPIPlus.git
+        git clone https://github.com/HsnSaboor/CLIProxyAPIPlus.git
         cd CLIProxyAPIPlus
     fi
 
